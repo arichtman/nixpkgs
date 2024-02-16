2fa7ae0e48f522ffc78780fc2e2768d062c6cecd
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-02-16 17:06:13 +1000

    wip

---
+++ compare.sh
@@ -0,0 +1,34 @@
+LAST_VERSION=0.54.0
+PACKAGE=terragrunt
+
+CURRENT_VERSION=$(nix eval --raw .#terragrunt.version)
+COMPARISON=$(nix eval --expr " builtins.compareVersions \"${LAST_VERSION}\" \"${CURRENT_VERSION}\"")
+[ $COMPARISON -eq -1 ];
+
+is_current_newer() {
+  local CURRENT=$(get_current_version "${PACKAGE}")
+  local COMPARISON=$(nix eval --expr " builtins.compareVersions \"${1}\" \"${CURRENT}\"")
+  [ $COMPARISON -eq -1 ];
+}
+get_current_version(){
+  nix eval --raw ".#${1}.version";
+}
+
+get_current_commit_hash(){
+  git log --format=format:"%H" --no-patch -1 ;
+}
+
+get_first_commit_hash(){
+  # git log --format=format:"%H" --no-patch --reverse | head -1 ;
+  git log -1000 --format=format:%H | tail -r | head -1 ;
+}
+
+git bisect start
+# I think bad/new aren't hanging it's just processing 55k+ commits
+# I think we can skip getting current commit hash
+# git bisect good $(get_first_commit_hash)
+# git bisect bad $(get_current_commit_hash)
+git bisect old $(get_first_commit_hash)
+git bisect new $(get_current_commit_hash)
+git bisect run is_current_newer "${LAST_VERSION}"
+# git tag "${PACKAGE}-$(get_current_version)"
